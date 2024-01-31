859041c725fc033f481825d532d3eecb9cdef94f
apizzimenti <apizzime@gmu.edu>
2024-01-31 08:48:05 -0500

    remove utils

--- potts/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .Metadata import Metadata
-from .coordinates import (
-    coordinates, elementwiseSubtract, subtractMany, binaryEncode, increment,
-    elementwiseAdd, binaryUnencode, flattenAndSortSetUnion, elementwiseAddOne
-)
-
-__all__ = ["Metadata", "coordinates"]
