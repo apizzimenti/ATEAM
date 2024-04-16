fix energy computation; improve replay speed

potts/models/Glauber.py
@@ -2,21 +2,20 @@
 import numpy as np
 from typing import Callable
 
-from ..arithmetic import linalg
 from ..structures import Lattice
 from ..stats import constant
 from .Model import Model
 
 
-class SwendsenWang(Model):
-    name = "SwendsenWang"
+class Glauber(Model):
+    name = "Glauber"
     
     def __init__(
             self, L: Lattice, temperatureFunction: Callable=constant(-0.6),
             initial=None
         ):
         """
-        Initializes Swendsen-Wang evolution on the Potts model.
+        Initializes Glauber dynamics on the Potts model.
 
         Args:
             L: The `Lattice` object on which we'll be running experiments.
@@ -30,6 +29,11 @@ class SwendsenWang(Model):
 
         # SW defaults.
         self.state = initial if initial else self.initial()
+        self.state = self.lattice.field(self.state)
+        self._shift = self.lattice.field(np.zeros(len(self.state), dtype=int))
+        self._shiftIndex = 0
+        self._indices = list(range(len(self.state)))
+
         self.spins = { face: self.state[self.lattice.index.faces[face]] for face in self.lattice.faces }
         self.occupied = set()
 
@@ -41,12 +45,14 @@ class SwendsenWang(Model):
         Returns:
             A NumPy array representing a vector of spin assignments.
         """
-        return np.array([np.random.randint(0, self.lattice.field.order) for _ in self.lattice.faces])
+        return self.lattice.field.Random(len(self.lattice.faces))
     
 
     def proposal(self, time):
         """
-        Proposal scheme for generalized Swendsen-Wang evolution on the Potts model.
+        Proposal scheme for generalized Glauber dynamics on the Potts model:
+        uniformly randomly chooses a face in the complex, flips the face's spin,
+        and returns the corresponding cocycle.
 
         Args:
             time (int): Step in the chain.
@@ -54,32 +60,15 @@ class SwendsenWang(Model):
         Returns:
             A NumPy array representing a vector of spin assignments.
         """
-        # Compute the probability of choosing any individual cube in the complex.
-        self.temperature = self.temperatureFunction(time)
-        p = 1-np.exp(self.temperature)
-        assert 0 <= p <= 1
+        # Choose a location to "flip," then revisit the last shifted vertex.
+        loc = np.random.randint(len(self.lattice.faces))
+        self._shift[self._shiftIndex] = 0
+        self._shift[loc] = self.lattice.field.Random()
+        self._shiftIndex = loc
 
-        # Choose cubes (i.e. columns) to include: we do so by asking whether the
-        # sum o f the faces is 0 and a weighted coin flip succeeds.
-        includeCubes = []
-        
-        for cube in self.lattice.cubes:
-            q = np.random.uniform()
-            null = self.lattice.field([self.spins[face] for face in cube.faces]).sum() == 0
+        return self.state+self._shift
 
-            if null and q < p:
-                includeCubes.append(cube)
-
-        includeCubeIndices = [self.lattice.index.cubes[cube] for cube in includeCubes]
-        self.occupied = set(includeCubes)
-
-        # Uniformly randomly sample a cocycle on the sublattice admitted by the
-        # chosen edges; reconstruct the labeling on the entire lattice by
-        # subbing in the values of c which differ from existing ones.
-        return linalg.sampleFromKernel(self.lattice.coboundary, self.lattice.field, includeCubeIndices)
-    
-
-    def assign(self, cocycle: np.array):
+    def assign(self, cocycle):
         """
         Updates mappings from faces to spins and cubes to occupations.
 
@@ -87,6 +76,7 @@ class SwendsenWang(Model):
             cocycle (np.array): Cocycle on the sublattice.
         """
         self.spins = { face: cocycle[self.lattice.index.faces[face]] for face in self.lattice.faces }
+        self.state = cocycle
         
         # Dual graph of sublattice of occupied cubes.
         # self.lattice.subgraph = self.lattice.graph.subgraph(

potts/models/InvadedCluster.py
@@ -8,25 +8,24 @@ from ..stats import constant
 from .Model import Model
 
 
-class SwendsenWang(Model):
-    name = "SwendsenWang"
+class InvadedCluster(Model):
+    name = "InvadedCluster"
     
     def __init__(
-            self, L: Lattice, temperatureFunction: Callable=constant(-0.6),
-            initial=None
+            self, L: Lattice, stoppingCondition: Callable=None, initial=None
         ):
         """
-        Initializes Swendsen-Wang evolution on the Potts model.
+        Initializes the invaded-cluster (invasion percolation?) MCMC model on
+        the given lattice.
 
         Args:
             L: The `Lattice` object on which we'll be running experiments.
-            temperatureFunction (Callable): A temperature schedule function which
-                takes a single positive integer argument `t`, and returns the
-                scheduled temperature at time `t`.
             initial (np.ndarray): A vector of spin assignments to components.
+            stoppingCondition (Callable): A rule which tells the proposal
+                algorithm to stopconstructing the current spin and edge configurations.
         """
         self.lattice = L
-        self.temperatureFunction = temperatureFunction
+        self.stoppingCondition = stoppingCondition
 
         # SW defaults.
         self.state = initial if initial else self.initial()
@@ -46,7 +45,8 @@ class SwendsenWang(Model):
 
     def proposal(self, time):
         """
-        Proposal scheme for generalized Swendsen-Wang evolution on the Potts model.
+        Proposal scheme for generalized invaded-cluster evolution on the Potts
+        model.
 
         Args:
             time (int): Step in the chain.
@@ -54,22 +54,18 @@ class SwendsenWang(Model):
         Returns:
             A NumPy array representing a vector of spin assignments.
         """
-        # Compute the probability of choosing any individual cube in the complex.
-        self.temperature = self.temperatureFunction(time)
-        p = 1-np.exp(self.temperature)
-        assert 0 <= p <= 1
+        # First, check whether we're reached the stopping condition; if so, we
+        # raise a StopIteration error to force the chain to stop.
+        if self.stoppingCondition(self): raise StopIteration
 
-        # Choose cubes (i.e. columns) to include: we do so by asking whether the
-        # sum o f the faces is 0 and a weighted coin flip succeeds.
-        includeCubes = []
-        
-        for cube in self.lattice.cubes:
-            q = np.random.uniform()
-            null = self.lattice.field([self.spins[face] for face in cube.faces]).sum() == 0
-
-            if null and q < p:
-                includeCubes.append(cube)
-
+        # Next, randomize the order of the cubes. Then, check whether the cubes
+        # in the configuration are sent to 0 by the current cochain; those that
+        # are are included unconditionally, and those that aren't are ignored.
+        randomized = np.shuffle(self.lattice.cubes)
+        includeCubes = [
+            cube for cube in randomized
+            if self.lattice.field([self.spins[face] for face in cube.faces]).sum() == 0
+        ]
         includeCubeIndices = [self.lattice.index.cubes[cube] for cube in includeCubes]
         self.occupied = set(includeCubes)
 
@@ -87,8 +83,3 @@ class SwendsenWang(Model):
             cocycle (np.array): Cocycle on the sublattice.
         """
         self.spins = { face: cocycle[self.lattice.index.faces[face]] for face in self.lattice.faces }
-        
-        # Dual graph of sublattice of occupied cubes.
-        # self.lattice.subgraph = self.lattice.graph.subgraph(
-        #     [self.lattice.index.cubes[cube] for cube in self.lattice.cubes if not cube in self.occupied]
-        # )

potts/stats/autocorrelation.py
@@ -1,25 +1,2 @@
 
-import numpy as np
-
-
-def autocorrelation(data, lag=500):
-    """
-    Computes the autocorrelation of a given observable over the provided time
-    lag.
-
-    Args:
-        data (Iterable): An iterable, indexed by sample times, containing data
-            from a given observable.
-        lag (int): How far back do we look?
-    """
-    # Expected value (i.e. sample mean, which converges to the expectation by
-    # LLN).
-    mu = data.mean()
-    normalized = data-mu
-    N = len(data)
-
-    autocorrs = np.array([
-        np.dot(normalized[:N-t], normalized[t:])*(1/N) for t in range(N-lag)
-    ])
-
-    return autocorrs/autocorrs[0]
+from ..arithmetic import autocorrelation