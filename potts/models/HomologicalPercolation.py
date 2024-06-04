94249d44c712c1a9dd978fcba83b73de2406c6d1
apizzimenti <apizzime@gmu.edu>
2024-06-04 11:23:38 -0400

    homological site percolation without giant cycle detection

---
+++ potts/models/HomologicalPercolation.py
@@ -0,0 +1,78 @@
+ 
+import numpy as np
+from typing import Callable
+
+from ..arithmetic import linalg
+from ..structures import Lattice
+from ..stats import constant
+from .Model import Model
+
+
+class HomologicalSitePercolation(Model):
+    name = "HomologicalSitePercolation"
+    
+    def __init__(
+            self, L: Lattice, temperatureFunction: Callable=constant(-0.6),
+            initial=None
+        ):
+        """
+        Initializes Swendsen-Wang evolution on the Potts model.
+
+        Args:
+            L: The `Lattice` object on which we'll be running experiments.
+            temperatureFunction (Callable): A temperature schedule function which
+                takes a single positive integer argument `t`, and returns the
+                scheduled temperature at time `t`.
+            initial (np.ndarray): A vector of spin assignments to components.
+        """
+        self.lattice = L
+        self.temperatureFunction = temperatureFunction
+
+        # SW defaults.
+        self.state = initial if initial else self.initial()
+        self.spins = { face: self.state[self.lattice.index.faces[face]] for face in self.lattice.faces }
+        self.occupied = set()
+
+
+    def initial(self) -> np.array:
+        """
+        Computes an initial state for the model's Lattice.
+
+        Returns:
+            A NumPy array representing a vector of spin assignments.
+        """
+        return np.array([np.random.randint(0, self.lattice.field.order) for _ in self.lattice.faces])
+    
+
+    def proposal(self, time):
+        """
+        Proposal scheme for generalized Swendsen-Wang evolution on the Potts model.
+
+        Args:
+            time (int): Step in the chain.
+
+        Returns:
+            A NumPy array representing a vector of spin assignments.
+        """
+        self.temperature = self.temperatureFunction(time)
+        p = 1-np.exp(self.temperature)
+        assert 0 <= p <= 1
+
+        # Assign each site "open" or "closed".
+        U = np.random.uniform(size=len(self.lattice.faces))
+        return (U < p).astype(int)
+    
+
+    def assign(self, cocycle: np.array):
+        """
+        Updates mappings from faces to spins and cubes to occupations.
+
+        Args: 
+            cocycle (np.array): Cocycle on the sublattice.
+        """
+        self.spins = { face: cocycle[self.lattice.index.faces[face]] for face in self.lattice.faces }
+        
+        # Dual graph of sublattice of occupied cubes.
+        # self.lattice.subgraph = self.lattice.graph.subgraph(
+        #     [self.lattice.index.cubes[cube] for cube in self.lattice.cubes if not cube in self.occupied]
+        # )
--- potts/models/__init__.py
+++ potts/models/__init__.py
@@ -5,9 +5,10 @@ from .SwendsenWang import SwendsenWang
 from .InvadedCluster import InvadedCluster
 from .GraphPercolation import GraphPercolation
 from .Glauber import Glauber
+from .HomologicalPercolation import HomologicalSitePercolation
 from .Model import Model
 
 __all__ = [
     "Model", "SwendsenWang", "GraphSwendsenWang", "GraphPercolation", "GraphIsing",
-    "InvadedCluster", "Glauber"
+    "InvadedCluster", "Glauber", "HomologicalSitePercolation"
 ]
