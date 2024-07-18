d2fb376a5607ae71428ff58b8b7358ab89bbd3a6
apizzimenti <apizzime@gmu.edu>
2024-07-18 16:33:54 -0400

    modernize development installation

--- setup.py
+++ setup.py
@@ -1,34 +1,22 @@
 
+from setuptools import setup
 from Cython.Build import cythonize
-from pathlib import Path
-from setuptools import find_packages, setup
-
-requirements = [
-    "numpy", "galois", "matplotlib", "pandas", "scipy", "rustworkx"
-]
-
-# Set the version --- ensure that the latest tag matches this value.
-VERSION = "0.0.1"
-
-# Description.
-here = Path(__file__).parent
-DESCRIPTION = (here / "README.md").read_text()
 
 setup(
-    name="potts",
-    version=VERSION,
-    author="Paul Duncan, Anthony E. Pizzimenti, Benjamin Schweinhart",
-    author_email="apizzime@gmu.edu",
-    description="Generalized Potts model.",
-    long_description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/apizzimenti/potts",
-    packages=find_packages(exclude=["test"]),
-    install_requires=requirements,
-    include_package_data=True,
-    extras_require={
-        "dev": ["pdoc3", "flake8", "pytest", "autopep8", "pytest-cov", "black", "isort"]
-    },
+    # name="potts",
+    # version=VERSION,
+    # author="Paul Duncan, Anthony E. Pizzimenti, Benjamin Schweinhart",
+    # author_email="apizzime@gmu.edu",
+    # description="Generalized Potts model.",
+    # long_description=DESCRIPTION,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/apizzimenti/potts",
+    # packages=find_packages(exclude=["test"]),
+    # install_requires=requirements,
+    # include_package_data=True,
+    # extras_require={
+    #     "dev": ["pdoc3", "flake8", "pytest", "autopep8", "pytest-cov", "black", "isort"]
+    # },
     ext_modules=cythonize(
         "potts/**/*.pyx"
     )
